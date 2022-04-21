f49ae4f04193d28aad6ae6f11951a35c94ec6fc2
iniewczas_252814 <iniewczas_252814@e-science.pl>
2022-04-21 21:54:53 +0200

    dodanie sterowania dzwiekiem

--- SystemController.py
+++ SystemController.py
@@ -1,10 +1,30 @@
 
-
+import platform
 import pyautogui
+try:
+    from ctypes import cast, POINTER
+    from comtypes import CLSCTX_ALL
+except ImportError:
+    pass
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 class SystemController(object):
 
+#scroll
     _scrollSpeed=200
+
+#windows volume
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volumeChange=0.6525
+
+
+    def __init__(self):
+        self.operating_system_name=platform.system()
+        self.currentVolume=self.volume.GetMasterVolumeLevel()
+
 
     @property
     def scrollSpeed(self):
@@ -19,4 +39,27 @@ class SystemController(object):
     def scrollDown(self, speed=_scrollSpeed):
         pyautogui.scroll(-self.scrollSpeed)
 
+
+
+    def windowsVolumeUp(self):
+        value=self.get_windowsVolume()+self.volumeChange
+        if value>0:
+            value=0
+        self.currentVolume=value
+        self.volume.SetMasterVolumeLevel(self.currentVolume, None)
+
+    def windowsVolumeDown(self):
+        value=self.get_windowsVolume()-self.volumeChange
+        if value<-65.25:
+            value=-65.25
+        self.currentVolume=value
+        self.volume.SetMasterVolumeLevel(self.currentVolume, None)
+
+
+    def get_windowsVolume(self):
+        return self.currentVolume
+
+
+    def set_linux_volume(v_value: float):
+        print("Place to function, which we have ")
 
--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import keyboard
 
 controller=SystemController()
 
+volume=float(controller.get_windowsVolume())
+print(volume)
 
 while True:
     key=0
     key=keyboard.read_key()
-    if key=='=':
+    if key=='1':
         controller.scrollUp()
-    elif key=='-':
+    elif key=='2':
         controller.scrollDown()
+    elif key=='3':
+        controller.windowsVolumeUp()
+    elif key=='4':
+        volume-=3
+        controller.windowsVolumeDown()
+    print (volume)
     print(key)
