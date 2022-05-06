524b15b596d58898b629c8296ee5240c0c6d7190
Mateusz <252808@student.pwr.edu.pl>
2022-05-06 13:38:49 +0200

    Dodanie klasy zarządającej kamerami podpiętymi do komputera

---
+++ controllers/camera_controller.py
@@ -0,0 +1,32 @@
+import cv2
+class ApplicationController():
+    def __init__(self):
+        self.camera_list=[]
+        self.camera_list=self.get_all_camera_from_system()
+        self.used_camera_number=self.camera_list[0]
+        self.cap=cv2.VideoCapture(self.used_camera_number)
+
+    def get_all_camera_from_system(self):
+        self.camera_list.clear()
+        for i in range(100):
+            cap = cv2.VideoCapture(i,cv2.CAP_DSHOW)
+            if cap.isOpened():
+                self.camera_list.append(i)
+                cap.release()
+        self.camera_list.pop(-1)
+        print(self.camera_list)
+
+    def get_used_camera_number(self):
+        return self.used_camera_number
+    #Klasy camera oraz mouse_steering powinny obraz z kamery pobierać przy pomocy tej funkcji
+    def get_camera_image(self):
+        ret, frame = self.cap.read()
+        return ret,frame
+    def get_all_cameras(self):
+        return self.camera_list
+    def set_used_camera_number(self,number):
+        self.used_camera_number=number
+
+if __name__ == '__main__':
+    ap=ApplicationController()
+    ap.get_all_camera_from_system
