261950344612890d409ac59d7ec65a3346006472
Mateusz <252808@student.pwr.edu.pl>
2022-05-12 09:08:33 +0200

    Włączanie rozpoznawania i wyłączanie z poziomu aplikacji działa poprawnie.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.insert(0, "..")
 from gesture_liblary.camera import Camera
+import threading
 class Controller():
     def __init__(self):
         self.camera=Camera()
     def start_gesture_recognition(self):
-        self.camera.start_windows_gesture_library()
+        self.t=threading.Thread(name='daemon',target=self.camera.start_windows_gesture_library)
+        self.t.start()
     def stop_gesture_recognition(self):
         self.camera.stop_gesture_recognition()
+        self.t.join()
